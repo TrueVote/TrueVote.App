b3d71e4d1955915ca34ee396e7582ae4fe75374e
Brett Morrison <b@brettmorrison.com>
2023-10-03 21:33:34 -0700

    AD-68 Ported first module to Mantine 7.x

--- src/ui/Hero.tsx
+++ src/ui/Hero.tsx
@@ -1,21 +1,8 @@
-import { createStyles, Stack, Text, Title } from '@mantine/core';
+import { Stack, Text, Title } from '@mantine/core';
 import { Helmet, HelmetData } from 'react-helmet-async';
+import classes from './Hero.module.css';
 
 const helmetData: any = new HelmetData({});
-
-const useStyles: any = createStyles((theme: any) => ({
-  title: {
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    fontSize: 44,
-    lineHeight: 1.2,
-    fontWeight: 900,
-
-    [theme.fn.smallerThan('xs')]: {
-      fontSize: 28,
-    },
-  },
-}));
 
 export const Hero: any = ({
   title,
@@ -24,7 +11,6 @@ export const Hero: any = ({
   title: React.ReactNode;
   subTitle?: React.ReactNode;
 }) => {
-  const { classes } = useStyles();
   return (
     <>
       <Helmet helmetData={helmetData}>
