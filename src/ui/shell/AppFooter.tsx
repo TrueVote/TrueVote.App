0a242efd4f10d7ab2d76ff54c4ab8f9e3df54571
Brett Morrison <b@brettmorrison.com>
2023-11-10 17:38:09 -0800

    AD-83 Fixed footer alignment issue on narrow devices

--- src/ui/shell/AppFooter.tsx
+++ src/ui/shell/AppFooter.tsx
@@ -1,23 +1,21 @@
 import classes from '@/ui/shell/AppStyles.module.css';
-import { Anchor, AppShell, Container, Group, Text } from '@mantine/core';
+import { Anchor, AppShell, Container, Text } from '@mantine/core';
 import { FC } from 'react';
 
 export const AppFooter: FC = () => {
   return (
     <AppShell.Footer>
       <Container fluid className={classes.footer}>
-        <Group gap={20}>
-          <Anchor href='//truevote.org' className={classes.link} target='_blank'>
-            <Text size={'xs'}>Copyright © 2023 TrueVote, Inc.</Text>
-          </Anchor>
-          <Anchor
-            href='//github.com/TrueVote/TrueVote.App/releases'
-            className={classes.link}
-            target='_blank'
-          >
-            <Text size={'xs'}>Version 1.0</Text>
-          </Anchor>
-        </Group>
+        <Anchor href='//truevote.org' className={classes.link} target='_blank'>
+          <Text size={'xs'}>Copyright © 2023 TrueVote, Inc.</Text>
+        </Anchor>
+        <Anchor
+          href='//github.com/TrueVote/TrueVote.App/releases'
+          className={classes.link}
+          target='_blank'
+        >
+          <Text size={'xs'}>Version 1.0</Text>
+        </Anchor>
       </Container>
     </AppShell.Footer>
   );
