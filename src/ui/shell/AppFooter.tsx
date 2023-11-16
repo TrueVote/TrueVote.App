551ef9691e7b99eecb513082c8a2dd6ff423e30f
Brett Morrison <b@brettmorrison.com>
2023-11-15 20:07:12 -0800

    AD-85 Added localization to footer

--- src/ui/shell/AppFooter.tsx
+++ src/ui/shell/AppFooter.tsx
@@ -1,20 +1,25 @@
+import { useGlobalContext } from '@/Global';
 import classes from '@/ui/shell/AppStyles.module.css';
 import { Anchor, AppShell, Container, Text } from '@mantine/core';
 import { FC } from 'react';
 
 export const AppFooter: FC = () => {
+  const { localization } = useGlobalContext();
+
   return (
     <AppShell.Footer>
       <Container fluid className={classes.footer}>
         <Anchor href='//truevote.org' className={classes.link} target='_blank'>
-          <Text size={'xs'}>Copyright © 2023 TrueVote, Inc.</Text>
+          <Text size={'xs'}>
+            {localization?.getLocalizedString('COPYRIGHT')} © 2023 TrueVote, Inc.
+          </Text>
         </Anchor>
         <Anchor
           href='//github.com/TrueVote/TrueVote.App/releases'
           className={classes.link}
           target='_blank'
         >
-          <Text size={'xs'}>Version 1.0</Text>
+          <Text size={'xs'}>{localization?.getLocalizedString('VERSION')} 1.0</Text>
         </Anchor>
       </Container>
     </AppShell.Footer>
