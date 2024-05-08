218283cb5b322f8286b39323bda2ac953d949d44
Brett Morrison <b@brettmorrison.com>
2024-05-08 16:53:15 -0700

    AD-109 Removed deprecated property not needed in <TrueVoteLoader />

--- src/ui/CustomLoader.tsx
+++ src/ui/CustomLoader.tsx
@@ -2,8 +2,8 @@
 /* eslint-disable react/display-name */
 /* eslint-disable react/prop-types */
 /* eslint-disable @typescript-eslint/typedef */
-import { LoadingOverlay, MantineLoaderComponent } from '@mantine/core';
 import { TrueVoteSpinner } from '@/ui/TrueVoteSpinner';
+import { LoadingOverlay, MantineLoaderComponent } from '@mantine/core';
 import { forwardRef } from 'react';
 
 interface TrueVoteLoaderProps {
@@ -14,10 +14,6 @@ export const TrueVoteLoader: React.FC<TrueVoteLoaderProps> = ({
   visible = true,
 }): React.JSX.Element => {
   return <LoadingOverlay visible={visible} overlayProps={{ blur: 2 }}></LoadingOverlay>;
-};
-
-TrueVoteLoader.defaultProps = {
-  visible: true,
 };
 
 export const TrueVoteSpinnerLoader: MantineLoaderComponent = forwardRef(() => {
