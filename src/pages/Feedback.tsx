1994d10a814fcf65a2160745904414d052d54b0a
Brett Morrison <b@brettmorrison.com>
2024-05-08 16:10:49 -0700

    AD-108 Added waiting spinner, new copy, and state management to Feedback UI

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -1,9 +1,10 @@
 import { useGlobalContext } from '@/Global';
 import { FeedbackModel, SecureString } from '@/TrueVote.Api';
 import { DBSaveFeedback } from '@/services/DataClient';
+import { TrueVoteLoader } from '@/ui/CustomLoader';
 import { Hero } from '@/ui/Hero';
 import classes from '@/ui/shell/AppStyles.module.css';
-import { Button, Container, Group, Stack, Text, Textarea } from '@mantine/core';
+import { Button, Container, Group, Space, Stack, Text, Textarea } from '@mantine/core';
 import { FC, useState } from 'react';
 
 export const Feedback: FC = () => {
@@ -11,11 +12,14 @@ export const Feedback: FC = () => {
   const [savedFeedback, setSavedFeedback] = useState('');
   const [isClicked, setIsClicked] = useState(false);
   const { userModel } = useGlobalContext();
+  const [visible, setVisible] = useState(false);
+  const [done, setDone] = useState(false);
 
   const submitFeedback: any = async () => {
     console.info('Feedback', feedback);
 
     setIsClicked(true);
+    setVisible((v: boolean) => !v);
     setSavedFeedback('Submitting Feedback');
 
     const feedbackModel: FeedbackModel = {
@@ -30,11 +34,14 @@ export const Feedback: FC = () => {
         console.info('DBSaveFeedback', res);
         setSavedFeedback('Feedback Submitted');
         setTimeout(() => setIsClicked(false), 3000);
+        setVisible((v: boolean) => !v);
+        setTimeout(() => setDone((d: boolean) => !d), 1000);
       })
       .catch((e: SecureString) => {
         console.error('Error from DBSaveUser', e);
         setSavedFeedback('Error saving feedback: ' + e.Value);
         setTimeout(() => setIsClicked(false), 3000);
+        setVisible((v: boolean) => !v);
       });
   };
 
@@ -43,27 +50,39 @@ export const Feedback: FC = () => {
       <Stack gap={32}>
         <Hero title='Feedback' />
       </Stack>
-      <Textarea
-        autosize
-        minRows={4}
-        maxRows={8}
-        maxLength={1024}
-        placeholder='Feedback'
-        value={feedback}
-        onChange={(event) => setFeedback(event.currentTarget.value)}
-      ></Textarea>
-      <Group>
-        <Button
-          variant='light'
-          color='blue'
-          mt='md'
-          radius='md'
-          onClick={(): void => submitFeedback()}
-        >
-          Submit Feedback
-        </Button>
-        <Text>{isClicked ? savedFeedback : ''}</Text>
-      </Group>
+      <TrueVoteLoader visible={visible} />
+      {done ? (
+        <>
+          <Text>Thank you for your feedback!</Text>
+        </>
+      ) : (
+        <>
+          <Space h='md'></Space>
+          <Text>We welcome all feedback. Please add your thoughts below.</Text>
+          <Space h='md'></Space>
+          <Textarea
+            autosize
+            minRows={4}
+            maxRows={8}
+            maxLength={1024}
+            placeholder='Feedback'
+            value={feedback}
+            onChange={(event) => setFeedback(event.currentTarget.value)}
+          ></Textarea>
+          <Group>
+            <Button
+              variant='light'
+              color='blue'
+              mt='md'
+              radius='md'
+              onClick={(): void => submitFeedback()}
+            >
+              Submit Feedback
+            </Button>
+            <Text>{isClicked ? savedFeedback : ''}</Text>
+          </Group>
+        </>
+      )}
     </Container>
   );
 };
