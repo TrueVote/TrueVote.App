07991347d4d54b7f88090df41c519e15c3ea71f5
Brett Morrison <b@brettmorrison.com>
2024-10-27 16:51:06 -0700

    AD-136 Fixed wording for Start / End date for Election window and Ballot Submission

--- src/pages/Ballot.tsx
+++ src/pages/Ballot.tsx
@@ -148,7 +148,7 @@ const Election: FC<ElectionProps> = ({ election, electionBallot, navigate, acces
     if (
       election.HeaderImageUrl !== undefined &&
       election.HeaderImageUrl?.length !== undefined &&
-      election.HeaderImageUrl?.length > 0
+      election.HeaderImageUrl?.length > 1
     ) {
       return (
         <Card.Section>
@@ -227,12 +227,14 @@ const Election: FC<ElectionProps> = ({ election, electionBallot, navigate, acces
         <Text size='xl'>{election.Name}</Text>
         <Group mt='md' mb='xs'>
           <Badge color='green' variant='light'>
-            Starts: {moment.utc(election.StartDate).local().format('MMMM DD, YYYY, HH:mm:ss')}
+            {new Date().toISOString() > election.StartDate ? 'Started: ' : 'Starts: '}
+            {moment.utc(election.StartDate).local().format('MMMM DD, YYYY, HH:mm:ss')}
           </Badge>
           <Badge color='pink' variant='light'>
-            Ends: {moment.utc(election.EndDate).local().format('MMMM DD, YYYY, HH:mm:ss')}
+            {new Date().toISOString() > election.EndDate ? 'Ended: ' : 'Ends: '}
+            {moment.utc(election.EndDate).local().format('MMMM DD, YYYY, HH:mm:ss')}
           </Badge>
-        </Group>
+        </Group>{' '}
         <Text size='sm' c='dimmed'>
           {election.Description}
         </Text>
@@ -265,19 +267,22 @@ const Election: FC<ElectionProps> = ({ election, electionBallot, navigate, acces
             {races as any}
           </Flex>
         </Card.Section>
-        <Button
-          variant='light'
-          color='green'
-          fullWidth
-          radius='md'
-          h={60}
-          size='xl'
-          className={classes.ballotButton}
-          disabled={!isSubmitEnabled}
-          onClick={(): void => submitBallot()}
-        >
-          Submit Ballot
-        </Button>
+        {new Date().toISOString() >= election.StartDate &&
+          new Date().toISOString() <= election.EndDate && (
+            <Button
+              variant='light'
+              color='green'
+              fullWidth
+              radius='md'
+              h={60}
+              size='xl'
+              className={classes.ballotButton}
+              disabled={!isSubmitEnabled}
+              onClick={(): void => submitBallot()}
+            >
+              Submit Ballot
+            </Button>
+          )}{' '}
       </Card>
     </Container>
   );
